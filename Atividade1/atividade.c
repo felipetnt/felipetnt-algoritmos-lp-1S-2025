94be32b07a0a6aa12f792257ae3214e96553da00
felipetnt <felipeashlei@gmail.com>
2025-03-28 11:21:44 -0300

    Shift

--- Atividade1/atividade.c
+++ Atividade1/atividade.c
@@ -9,16 +9,16 @@ int main(){
     scanf("%d", &idade);
     printf("Insira sua peso: ");
     scanf("%d", &peso);
-    printf("Insira sua idade: ");
+    printf("Insira sua altura: ");
     scanf("%d", &altura);
 
     // IMC - altura sobre peso ao quadrado;
 
     float IMC = altura / (peso * peso);
 
-    printf("Voce tem %d anos.\n");
-    printf("Sua altura eh %.2f.\n");
-    printf("Seu peso eh %.2f.\n");
+    printf("Voce tem %d anos.\n", idade);
+    printf("Sua altura eh %.2f.\n", altura);
+    printf("Seu peso eh %.2f.\n", peso);
 
     return 0;
 }
