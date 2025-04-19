5b42bfd4318e99fc9b0970d651da88b85a6c5834
felipetnt <felipeashlei@gmail.com>
2025-04-18 22:35:18 -0300

    aperfeicoamentos...

--- Atividade5/atividade.c
+++ Atividade5/atividade.c
@@ -7,9 +7,21 @@ int main(){
     float somaDasNotas = 0;
     float mediaDasNotas;
     int qtdAlunosAprovados = 0;
+    float menorNota = 10;
+    int k;
 
     printf("Insira a quantidade de alunos que tem na sua turma: \n");
     scanf("%d", &qtdAlunosTurma);
+
+    do{
+        printf("Insira qual o aluno da posicao K voce deseja: \n");
+        scanf("%d", &k);
+
+        if(k> qtdAlunosTurma){
+            printf("Por favor insira um aluno dentro da sequencia de alunos...");
+        }
+
+    } while(k > qtdAlunosTurma);
 
     float notas[qtdAlunosTurma];
 
@@ -23,7 +35,7 @@ int main(){
 
             if(0 > notas[i] || notas[i] > 10){
                 
-                printf("Insira um valor valido... ");
+                printf("Insira uma nota valida dentre as notas... ");
 
             } else {
 
@@ -33,6 +45,10 @@ int main(){
 
                 if(notas[i] >= 6){
                     qtdAlunosAprovados++;
+                }
+                
+                if(menorNota > notas[i]){
+                    menorNota = notas[i];
                 }
 
                 somaDasNotas += notas[i];
@@ -45,8 +61,11 @@ int main(){
     mediaDasNotas = somaDasNotas / qtdAlunosTurma;
 
     printf("\nA maior nota da sala foi: %.1f", maiorNota);
+    printf("\nA menor nota da sala foi:: %.1f", menorNota);
     printf("\nA media das notas dos alunos: %.1f", mediaDasNotas);
     printf("\nA quantidade de alunos aprovados foi: %d", qtdAlunosAprovados);
+    printf("\nA nota do aluno K eh:  %.1f", notas[k]);
+    
 
     return 0;
 }   
